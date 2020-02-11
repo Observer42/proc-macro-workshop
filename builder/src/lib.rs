dc41f62d8835b5dd55d5cb24f7bcec7e505ebe3b
Observer42 <yishengxu47@gmail.com>
2020-02-12 01:21:52 +0800

    Derive Builder test 02

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,13 +1,60 @@
 extern crate proc_macro;
 
 use proc_macro::TokenStream;
-use syn::{parse_macro_input, DeriveInput};
 use quote::quote;
+use syn::{parse_macro_input, DeriveInput, Ident};
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
     let ast = parse_macro_input!(input as DeriveInput);
-    eprintln!("{:#?}", ast);
+    //eprintln!("{:#?}", ast);
 
-    TokenStream::new()
+    let type_name = &ast.ident;
+    let builder_name = Ident::new(&format!("{}Builder", type_name), type_name.span());
+    let vis = &ast.vis;
+
+    let fields = if let syn::Data::Struct(syn::DataStruct {
+        fields: syn::Fields::Named(syn::FieldsNamed { ref named, .. }),
+        ..
+    }) = ast.data
+    {
+        named
+    } else {
+        unimplemented!()
+    };
+
+    let optionized = fields.iter().map(|field| {
+        let name = &field.ident;
+        let ty = &field.ty;
+        quote! {
+            #name: std::option::Option<#ty>
+        }
+    });
+
+    let initial_values = fields.iter().map(|field| {
+        let name = &field.ident;
+        quote! {
+            #name: None
+        }
+    });
+
+    //eprintln!("{:#?}", fields);
+
+    let expanded = quote! {
+        #vis struct #builder_name {
+            #(#optionized,)*
+        }
+
+        impl #type_name {
+            #vis fn builder() -> #builder_name {
+                #builder_name {
+                    #(#initial_values,)*
+                }
+            }
+        }
+    };
+
+    //eprintln!("{:#?}", expanded);
+
+    TokenStream::from(expanded)
 }
