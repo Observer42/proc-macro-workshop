fbaad852f46e50ec1af94479a68183d7b8f620b0
Observer42 <yishengxu47@gmail.com>
2020-02-17 02:07:00 +0800

    Derive Builder test 07

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,10 +1,12 @@
+#![recursion_limit = "128"]
+
 extern crate proc_macro;
 
 use proc_macro::TokenStream;
 use quote::quote;
 use syn::{parse_macro_input, DeriveInput, Ident};
 
-#[proc_macro_derive(Builder)]
+#[proc_macro_derive(Builder, attributes(builder))]
 pub fn derive(input: TokenStream) -> TokenStream {
     let ast = parse_macro_input!(input as DeriveInput);
     //eprintln!("{:#?}", ast);
@@ -20,28 +22,26 @@ pub fn derive(input: TokenStream) -> TokenStream {
     {
         named
     } else {
-        unimplemented!()
+        panic!("currently only support struct with named fields");
     };
     //eprintln!("{:#?}", fields);
 
-    let optionized = fields.iter().map(|field| {
+    let builder_fields = fields.iter().map(|field| {
         let name = &field.ident;
         let ty = &field.ty;
-        if is_option_ty(ty) {
-            quote! {
-                #name: #ty
-            }
+        if is_option_ty(ty) || builder_attr(&field).is_some() {
+            quote! { #name: #ty }
         } else {
-            quote! {
-                #name: std::option::Option<#ty>
-            }
+            quote! { #name: std::option::Option<#ty> }
         }
     });
 
     let initial_values = fields.iter().map(|field| {
         let name = &field.ident;
-        quote! {
-            #name: None
+        if builder_attr(&field).is_some() {
+            quote! { #name: std::vec::Vec::new() }
+        } else {
+            quote! { #name: std::option::Option::None }
         }
     });
 
@@ -51,10 +51,39 @@ pub fn derive(input: TokenStream) -> TokenStream {
         if is_option_ty(ty) {
             ty = unwrap_option_ty(ty);
         }
-        quote! {
-            #vis fn #name(&mut self, #name: #ty) -> &mut Self {
-                self.#name = Some(#name);
-                self
+        if let Some(attr) = builder_attr(&field) {
+            match &parse_attr(attr) {
+                Ok(each_name) => {
+                    let each_ty = unwrap_ty(ty);
+                    if name.as_ref().unwrap() == each_name {
+                        quote! {
+                            #vis fn #each_name(&mut self, #each_name: #each_ty) -> &mut Self {
+                                self.#name.push(#each_name);
+                                self
+                            }
+                        }
+                    } else {
+                        quote! {
+                            #vis fn #name(&mut self, #name: #ty) -> &mut Self {
+                                self.#name = #name;
+                                self
+                            }
+
+                            #vis fn #each_name(&mut self, #each_name: #each_ty) -> &mut Self {
+                                self.#name.push(#each_name);
+                                self
+                            }
+                        }
+                    }
+                }
+                Err(_) => panic!("invalid attr"),
+            }
+        } else {
+            quote! {
+                #vis fn #name(&mut self, #name: #ty) -> &mut Self {
+                    self.#name = std::option::Option::Some(#name);
+                    self
+                }
             }
         }
     });
@@ -62,20 +91,16 @@ pub fn derive(input: TokenStream) -> TokenStream {
     let build_fields = fields.iter().map(|field| {
         let name = &field.ident;
         let ty = &field.ty;
-        if is_option_ty(ty) {
-            quote! {
-                #name: self.#name.clone()
-            }
+        if is_option_or_vec_ty(ty) {
+            quote! { #name: self.#name.clone() }
         } else {
-            quote! {
-                #name: self.#name.clone().ok_or(concat!(stringify!(#name), " is not set"))?
-            }
+            quote! { #name: self.#name.clone().ok_or(concat!(stringify!(#name), " is not set"))? }
         }
     });
 
     let expanded = quote! {
         #vis struct #builder_name {
-            #(#optionized,)*
+            #(#builder_fields,)*
         }
 
         impl #type_name {
@@ -89,7 +114,7 @@ pub fn derive(input: TokenStream) -> TokenStream {
         impl #builder_name {
             #(#builder_methods)*
 
-            #vis fn build(&mut self) -> Result<#type_name, Box<dyn std::error::Error>> {
+            #vis fn build(&mut self) -> std::result::Result<#type_name, std::boxed::Box<dyn std::error::Error>> {
                 Ok(#type_name {
                     #(#build_fields,)*
                 })
@@ -109,8 +134,53 @@ fn is_option_ty(ty: &syn::Type) -> bool {
     false
 }
 
+fn is_option_or_vec_ty(ty: &syn::Type) -> bool {
+    if let syn::Type::Path(ref tp) = ty {
+        if tp.path.segments.len() == 1 {
+            let ident = &tp.path.segments[0].ident;
+            return ident == "Option" || ident == "Vec";
+        }
+    }
+    false
+}
+
+fn builder_attr(ty: &syn::Field) -> Option<&syn::Attribute> {
+    for attr in &ty.attrs {
+        if attr.path.segments.len() == 1 && attr.path.segments[0].ident == "builder" {
+            return Some(&attr);
+        }
+    }
+    None
+}
+
+fn parse_attr(attr: &syn::Attribute) -> Result<syn::Ident, Box<dyn std::error::Error>> {
+    let meta = attr.parse_meta()?;
+    let lit = match meta {
+        syn::Meta::List(meta_list) => {
+            if meta_list.nested.len() != 1 {
+                Err("wrong attr format!".into())
+            } else {
+                if let syn::NestedMeta::Meta(syn::Meta::NameValue(nv)) = &meta_list.nested[0] {
+                    Ok(nv.lit.clone())
+                } else {
+                    Err("wrong attr format!".into())
+                }
+            }
+        }
+        _ => Err("wrong attr format!".to_owned()),
+    }?;
+    match lit {
+        syn::Lit::Str(s) => Ok(syn::Ident::new(&s.value(), s.span())),
+        _ => Err("wrong attr format!".into()),
+    }
+}
+
 fn unwrap_option_ty(ty: &syn::Type) -> &syn::Type {
     assert!(is_option_ty(ty));
+    unwrap_ty(ty)
+}
+
+fn unwrap_ty(ty: &syn::Type) -> &syn::Type {
     if let syn::Type::Path(ref tp) = ty {
         if let syn::PathArguments::AngleBracketed(ref bracketed_args) =
             tp.path.segments[0].arguments
